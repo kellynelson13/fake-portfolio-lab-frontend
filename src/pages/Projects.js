2bbb2f1a242cf7d8dd2bc93240289e0fcc1b3270
Kelly Nelson <kelnel86@gmail.com>
2022-09-27 19:30:38 -0500

    finishes frontend

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 const Projects = (props) => {
-  return (
-    <div>Projects</div>
-  )
-}
+  const [projects, setProjects] = useState(null);
+
+  const getProjects = async () => {
+    const response = await fetch(props.URL + "projects");
+    const data = await response.json();
+    setProjects(data);
+  };
+
+  useEffect(() => {
+    getProjects()
+  }, [])
+
+  const loaded = () => {
+    return projects.map((proj) => {
+      const { name, live, git, image } = proj;
+      return (
+        <div>
+          <h1>{name}</h1>
+          <a href={git}>
+          <button>Github</button>
+        </a>
+        <a href={live}>
+          <button>Live Site</button>
+        </a>
+          <img style={{width:"90%"}}src={image} alt={name}/>
+        </div>
+      );
+    });
+  };
+
+  return projects ? loaded() : <h1>Loading . . .</h1>
+};
 
 export default Projects;

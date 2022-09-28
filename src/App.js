5dbc793f31bb5acbca90124155ebdce5a1de6ca7
Kelly Nelson <kelnel86@gmail.com>
2022-09-27 19:15:03 -0500

    finished about page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import About from './pages/About';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import { Route, Switch } from "react-router-dom";
 
 function App() {
+
+  const URL = "https://ga-fake-portfolio.herokuapp.com/"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/projects">
+          <Projects URL={URL}/>
+        </Route>
+        <Route path="/about">
+          <About URL={URL}/>
+        </Route>
+      </Switch>
+      <Footer />
+      
     </div>
   );
 }
